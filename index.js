5af5977e23ac8f9f7df0ce41729ce14bf96d94c6
Eman Gurung <egurung1117@gmail.com>
2023-07-02 21:53:05 -0400

    completed

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(a) {
+    a++;
+    return a;
+}
+
+function decrement(b) {
+    b--;
+    return b;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
